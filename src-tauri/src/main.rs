4a9f59542433049111dc7571f1460fe381487bf9
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-04-20 13:20:37 +0800

    fix: adding systray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,25 @@
+
 #![cfg_attr(
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
 
+//use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem,SystemTray};
 fn main() {
+
+/*	let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+	let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+
+	let tray_menu = SystemTrayMenu::new()
+ 	 .add_item(quit)
+  	.add_native_item(SystemTrayMenuItem::Separator)
+  	.add_item(hide);
+
+  let system_tray = SystemTray::new()
+    .with_menu(tray_menu);
+*/		
   tauri::Builder::default()
+		//.system_tray(system_tray)
     .plugin(tauri_plugin_store::Builder::default().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
